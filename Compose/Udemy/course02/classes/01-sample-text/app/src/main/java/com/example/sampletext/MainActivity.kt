6de948450fc91e8757d7c8202d210c9ca7448a5f
Pierre Vieira <49538805+PierreVieira@users.noreply.github.com>
2021-09-08 21:58:37 -0300

    styling text

--- Compose/Udemy/course02/classes/01-sample-text/app/src/main/java/com/example/sampletext/MainActivity.kt
+++ Compose/Udemy/course02/classes/01-sample-text/app/src/main/java/com/example/sampletext/MainActivity.kt
@@ -3,11 +3,18 @@ package com.example.sampletext
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.sampletext.ui.theme.SampleTextTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +24,7 @@ class MainActivity : ComponentActivity() {
             SampleTextTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Pierre Vieira")
+                    GreetingText("world")
                 }
             }
         }
@@ -25,14 +32,23 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun GreetingText(name: String) {
+    Text(
+        text = "Hello $name",
+        modifier = Modifier
+            .clickable { }
+            .padding(24.dp)
+            .width(200.dp)
+            .height(240.dp),
+        style = MaterialTheme.typography.h5,
+        fontWeight = FontWeight.SemiBold
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     SampleTextTheme {
-        Greeting("Pierre Vieira")
+        GreetingText("world")
     }
 }
