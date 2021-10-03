da1e789ccca14f0ec0784733e54f83b55684394a
Pierre Vieira <49538805+PierreVieira@users.noreply.github.com>
2021-10-03 16:55:40 -0300

    Column, rows and size

---
+++ Compose/PhillipLackner/02-RowsColumnsBasicSizing/app/src/main/java/com/example/rowandcolumn/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.rowandcolumn
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Row(
+                modifier = Modifier
+                    .width(300.dp)
+                    .fillMaxHeight(0.7f)
+                    .background(color = Color.Green),
+                horizontalArrangement = Arrangement.SpaceAround,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text("Hello")
+                Text("World")
+                Text("Hello")
+            }
+        }
+    }
+}
