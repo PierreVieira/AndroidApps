ef70b59822ba5ce9a869a58ef54376f4e05f1a77
Pierre Vieira <49538805+PierreVieira@users.noreply.github.com>
2021-10-03 18:24:23 -0300

    modifier

---
+++ Compose/PhillipLackner/03-Modifier/app/src/main/java/com/example/modifier/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.modifier
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Column(
+                modifier = Modifier
+                    .background(Color.Green)
+                    .fillMaxHeight(0.5f)
+                    .fillMaxWidth()
+                    .border(5.dp, Color.Magenta)
+                    .padding(5.dp)
+                    .border(5.dp, Color.Blue)
+                    .padding(5.dp)
+                    .border(5.dp, Color.Red)
+                    .padding(5.dp)
+            ) {
+                Text("Hello")
+                Spacer(modifier = Modifier.height(40.dp))
+                Text("World")
+            }
+        }
+    }
+}
